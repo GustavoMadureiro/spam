37704d9032a25ed3ab8a0f728c1819c7778a948f
GustavoMadureiro <54814519+GustavoMadureiro@users.noreply.github.com>
2023-03-28 11:21:05 -0300

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 $ip_address = $_SERVER['REMOTE_ADDR'];
-$log_file = "logs.txt";
+$log_file = "log.txt";
 $log_entry = date('Y-m-d H:i:s') . " - $ip_address\n";
 file_put_contents($log_file, $log_entry, FILE_APPEND);
 header("Location: next_page.php");
